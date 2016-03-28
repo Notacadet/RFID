0648561ac44e11899c8dff40dc3860111fc08c2b
Ryan Brown (D Hour) <ryan.brown@usma.edu>
2016-03-27 21:13:07 -0400

    Display the header on live Search Pages

--- src/liveItem.php
+++ src/liveItem.php
@@ -1,6 +1,26 @@
 ﻿<html>
 <head>
-Search
+<meta charset=UTF-8>
+		<title>EECS Asset Management System</title>
+		<meta name=viewport content='width=device-width, initial-scale=1'>
+		<link rel=stylesheet href=navMenu.css>
+		</head>
+		<body>
+		<h1>EECS Asset Management System</h1>
+		<label for=show-menu class=show-menu>Show Menu</label>
+		<input type=checkbox id=show-menu role=button>
+		<ul id=menu>
+		<li><a href=homepage.php>Home</a></li>
+		<li><a href=handReciptHolderPage.php>Hand Receipts</a></li>
+		<li><a href=liveItem.php>Search by Item</a></li>
+		<li><a href=liveMakeAndModel.php>Search by Make and Model</a></li>
+		<li><a href=liveUser.php>Search User</a></li>
+		<li><a href=insertModelPage.php>Create a Make and a Model</a></li>
+		<li><a href=insertUserPage.php>Create User</a></li>
+		<li><a href=#>Latest Update</a></li>
+		<li><a href=#liveItem.php>Create Item</a></li>
+
+		</ul>
 <script type="text/javascript">
 var rfid='';
 var serialNum='';

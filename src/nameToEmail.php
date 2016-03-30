6f56117c17558931cde447b873d9db0f87585fd2
Eva Sung <eva.sung@usma.edu>
2016-03-30 11:19:38 -0400

    updateate

--- src/nameToEmail.php
+++ src/nameToEmail.php
@@ -1,17 +1,24 @@
 <?php
 
 	include 'handReceipt.php';
+	include 'RfidController.php';
+
 		
 	//isset($_GET['fn']!=="John Doe" )
 	$name=$_GET['fn'];
-
+	echo $name;
 	//Get the userName (email) based off of first and last name
 	$split=explode(" ",$name);
 	$fName=$split[0];
-	$lName=$split[1]; 
-	$sName = "SELECT userName FROM users WHERE firstName=$fName and lastName=$lName";
+	$lName=$split[1];
 	
+	$conn = RfidController::connect(); 
+	$sName = "SELECT userName FROM users WHERE firstNAme='$fName' and lastName='$lName'";
+	echo $sName;
+	$result = $conn->query($sName);
+	
+	//echo $result;
 	$testObject = new handReceipt();
-	$testObject->createHR("James Beck");
+	$testObject->createHR($result);
 	
 ?>

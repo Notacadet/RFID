ad023220a86f66c7eeace5375c5017d158a0acd2
Cedrik Bell <cedrik.bell@usma.edu>
2016-04-19 00:54:17 -0400

    Sessions Monster got me agian

--- src/session.php
+++ src/session.php
@@ -7,9 +7,9 @@ session_start();// Starting Session
 // Storing Session
 $user_check=$_SESSION['login_user'];
 // SQL Query To Fetch Complete Information Of User
-$ses_sql=mysqli_query($connection,"select userName from users where userName='$user_check'");
+$ses_sql=mysqli_query($connection,"select adminName from admin where adminName='$user_check'");
 $row = mysqli_fetch_assoc($ses_sql);
-$login_session =$row['userName'];
+$login_session =$row['adminName'];
 if(!isset($login_session)){
 mysqli_close($connection); // Closing Connection
 header('Location: index.php'); // Redirecting To Home Page

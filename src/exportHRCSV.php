2171ecb15e6e9df8e17343b75b50e8a2b5e7799a
aclamp <alexander.clamp@usma.edu>
2016-04-27 10:54:38 -0400

    update for gun
    Has all the data for use with the RFID gun

--- src/exportHRCSV.php
+++ src/exportHRCSV.php
@@ -19,22 +19,26 @@ class XCSV{
 		$conn = new mysqli($servername,$username,$password,$dbname);
 
 		// Fetch the data
-		$sql = "SELECT users.userName, nomenclature.nomenclature_Name, locations.roomNumber, makes.makeName, models.model_Name, items.rfid, items.serialNum FROM items join locations on items.location_id=locations.location_id join models on items.model_id=models.model_id join nomenclature on nomenclature.nomenclature_id=models.nom_id join makes on models.make_id=makes.make_id join users on users.user_id=items.hrholder_id WHERE userName like '%$selectedName%'";
+		$sql = "SELECT users.userName, nomenclature.nomenclature_Name, locations.roomNumber, makes.makeName, models.model_Name, items.rfid, items.serialNum, items.comments, items.accountedFor, items.dateAcquired, items.price, items.pbhrNumber FROM items join locations on items.location_id=locations.location_id join models on items.model_id=models.model_id join nomenclature on nomenclature.nomenclature_id=models.nom_id join makes on models.make_id=makes.make_id join users on users.user_id=items.hrholder_id WHERE userName like '%$selectedName%' ORDER BY rfid, nomenclature_Name, roomNumber, makeName, model_name";
 		$result = $conn->query($sql);
-
+		if (!$result){
+			die("Didn't work" . mysqli_error($conn));
+		}
 		// Headers for the file
-		fputcsv($file, array('Nomenclature', 'Count', 'Location','Make','Model','Serial Number','RFID'));
+		 fputcsv($file, array('RFID', 'Nomenclature', 'Location','Last Seen','State','State Last','Barcode','Make','Model','Serial Number','HR Holder','Comments','Computer Name','Last Cal','Cal Due','Acq Date','Price','PB HR/LIN','HR Signed'));
 		// Place the data in the file 
 		if ($result->num_rows > 0){
 					// output data of each row
 					while($row = $result->fetch_assoc()) {
-						fputcsv($file, array($row["nomenclature_Name"], "Count Holder",$row["roomNumber"], $row["makeName"], $row["model_Name"], $row["rfid"], $row["serialNum"]));
+						fputcsv($file, array($row["rfid"],$row["nomenclature_Name"],$row["roomNumber"],$row["accountedFor"],"itemstatus","","barcode",$row["makeName"],$row["model_Name"],$row["serialNum"],$row["userName"],$row["comments"],"","","",$row["dateAcquired"],$row["price"],$row["pbhrNumber"],""));
 					}
-				} 
+				}  
 		  
 
 		fclose($file);  
 	}
 }
+//$testObject = new XCSV();
 
+//$testObject->createCSV('Kyle.Moses@usma.edu');
 ?>
